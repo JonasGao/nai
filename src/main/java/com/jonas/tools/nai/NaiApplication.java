1764a5d985c77c6c4d937502c824f0ecaa233365
Jonas Gao <gaopinsong@live.cn>
2024-02-07 15:29:48 +0800

    Init

---
+++ src/main/java/com/jonas/tools/nai/NaiApplication.java
@@ -0,0 +1,13 @@
+package com.jonas.tools.nai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jonas/tools/nai/NaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonas.tools.nai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
