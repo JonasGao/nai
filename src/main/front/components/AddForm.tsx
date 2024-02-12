51c264b8a681bb9209e2912973c1ade97ab2c528
Jonas Gao <gaopinsong@live.cn>
2024-02-12 09:50:26 +0800

    feat(Frontend): New Date before submit.

--- src/main/front/components/AddForm.tsx
+++ src/main/front/components/AddForm.tsx
@@ -1,54 +1,84 @@
 "use client";
 
-import {Box, Button, FormControl, FormControlLabel, Radio, RadioGroup, TextField} from "@mui/material";
-import React, {useMemo, useState} from "react";
-import FormFields, {Operation} from "./FormFields";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  TextField,
+} from "@mui/material";
+import React, { useMemo, useState } from "react";
+import FormFields, { Operation } from "./FormFields";
 import dayjs from "dayjs";
-import {FeedingRecord} from "./Item";
-import {useRouter} from "next/navigation";
-import AlertDialog, {AlertErrorDetail} from "./AlertDialog";
+import { useRouter } from "next/navigation";
+import { AlertErrorDetail } from "./AlertDialog";
 
 type AddFeedingRecord = {
-  time: string, operation: Operation, value1: number, value2: number | null
-}
+  time: string;
+  operation: Operation;
+  value1: number;
+  value2: number | null;
+};
 
 export default function AddForm() {
-  const router = useRouter()
-  const [operation, setOperation] = useState<Operation>(`BREAST_MILK`)
-  const [datetime, setDatetime] = useState(new Date())
-  const datetimeValue = useMemo(() =>
-    dayjs(datetime).format("YYYY-MM-DDTHH:mm:ss"), [datetime])
-  const handleDatetimeChange = useMemo(() => (e: React.ChangeEvent<HTMLInputElement>) => {
-    setDatetime(dayjs(e.target.value).toDate())
-  }, [setDatetime])
-  const handleOperationChange = useMemo(() => (e: React.ChangeEvent<HTMLInputElement>, value: string) => {
-    setOperation(value as Operation)
-  }, [setOperation])
-  const [[value1, value2], setValues] = useState<[number, number | null]>([0, null])
-  const handleValuesChange = useMemo(() => (value1: number, value2: number | null) => {
-    setValues([value1, value2])
-  }, [setValues])
-  const handleSubmit = useMemo(() => () => {
-    fetch("/api/feeding-record", {
-      method: "POST",
-      headers: [
-        ["Content-Type", "application/json"]
-      ],
-      body: JSON.stringify({
-        operation: operation,
-        value1: value1,
-        value2: value2,
-        time: datetime.toISOString(),
-      })
-    }).then(resp => {
-      if (resp.ok) {
-        setDatetime(new Date())
-        router.refresh()
-      } else {
-        document.dispatchEvent(new CustomEvent<AlertErrorDetail>("alert-error", {detail: {message: "提交失败了！"}}))
-      }
-    })
-  }, [operation, value1, value2, datetime])
+  const router = useRouter();
+  const [operation, setOperation] = useState<Operation>(`BREAST_MILK`);
+  const [datetime, setDatetime] = useState<Date>();
+  const datetimeValue = useMemo(
+    () => datetime && dayjs(datetime).format("YYYY-MM-DDTHH:mm:ss"),
+    [datetime],
+  );
+  const handleDatetimeChange = useMemo(
+    () => (e: React.ChangeEvent<HTMLInputElement>) => {
+      setDatetime(dayjs(e.target.value).toDate());
+    },
+    [setDatetime],
+  );
+  const handleOperationChange = useMemo(
+    () => (e: React.ChangeEvent<HTMLInputElement>, value: string) => {
+      setOperation(value as Operation);
+    },
+    [setOperation],
+  );
+  const [[value1, value2], setValues] = useState<[number, number | null]>([
+    0,
+    null,
+  ]);
+  const handleValuesChange = useMemo(
+    () => (value1: number, value2: number | null) => {
+      setValues([value1, value2]);
+    },
+    [setValues],
+  );
+  const handleSubmit = useMemo(
+    () => () => {
+      fetch("/api/feeding-record", {
+        method: "POST",
+        headers: [["Content-Type", "application/json"]],
+        body: JSON.stringify({
+          operation: operation,
+          value1: value1,
+          value2: value2,
+          time: (datetime || new Date()).toISOString(),
+        }),
+      }).then((resp) => {
+        if (resp.ok) {
+          setValues([0, null]);
+          setDatetime(undefined);
+          router.refresh();
+        } else {
+          document.dispatchEvent(
+            new CustomEvent<AlertErrorDetail>("alert-error", {
+              detail: { message: "提交失败了！" },
+            }),
+          );
+        }
+      });
+    },
+    [operation, value1, value2, datetime],
+  );
   return (
     <Box>
       <FormControl>
@@ -58,19 +88,46 @@ export default function AddForm() {
           row
           name="operations"
         >
-          <FormControlLabel value="BREAST_MILK" control={<Radio/>} label="母乳"/>
-          <FormControlLabel value="BOTTLE_FEEDING" control={<Radio/>} label="瓶喂"/>
-          <FormControlLabel value="MILK_POWDER" control={<Radio/>} label="奶粉"/>
-          <FormControlLabel value="BIG_ONE" control={<Radio/>} label="大号"/>
-          <FormControlLabel value="LITTLE_ONE" control={<Radio/>} label="小号"/>
+          <FormControlLabel
+            value="BREAST_MILK"
+            control={<Radio />}
+            label="母乳"
+          />
+          <FormControlLabel
+            value="BOTTLE_FEEDING"
+            control={<Radio />}
+            label="瓶喂"
+          />
+          <FormControlLabel
+            value="MILK_POWDER"
+            control={<Radio />}
+            label="奶粉"
+          />
+          <FormControlLabel value="BIG_ONE" control={<Radio />} label="大号" />
+          <FormControlLabel
+            value="LITTLE_ONE"
+            control={<Radio />}
+            label="小号"
+          />
         </RadioGroup>
       </FormControl>
       <Box component={"form"}>
-        <TextField id="time" type="datetime-local" label="Time" variant="standard" value={datetimeValue}
-                   onChange={handleDatetimeChange}/>
-        <FormFields operation={operation} value1={value1} value2={value2} onChange={handleValuesChange}/>
+        <TextField
+          id="time"
+          type="datetime-local"
+          label="Time"
+          variant="standard"
+          value={datetimeValue}
+          onChange={handleDatetimeChange}
+        />
+        <FormFields
+          operation={operation}
+          value1={value1}
+          value2={value2}
+          onChange={handleValuesChange}
+        />
       </Box>
       <Button onClick={handleSubmit}>提交</Button>
     </Box>
-  )
+  );
 }
